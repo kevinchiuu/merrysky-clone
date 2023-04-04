dc667b34576527bff769397f2d6ab70313fb7017
Kevin Chiu <keavenchiu@gmail.com>
2023-04-03 23:16:15 -0400

    rounded box

--- src/components/Chart.svelte
+++ src/components/Chart.svelte
@@ -1,46 +1,8 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import Chart from 'chart.js/auto'
 
-    function chart() {
-        const ctx = <HTMLCanvasElement>document.getElementById('myChart');
-  
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-            labels: ['Monday'],
-            datasets: [{
-                label: '# of Votes',
-                data: [3],
-                borderWidth: 1
-            },{
-                label: '# of Votes',
-                data: [8],
-                borderWidth: 1
-            },{
-                label: '# of Votes',
-                data: [19],
-                borderWidth: 1
-            }]
-            },
-            options: {
-                indexAxis: 'y',
-                scales: {
-                    x:{
-                        stacked: true
-                    },
-                    y: {
-                        beginAtZero: true,
-                        stacked: true
-                    }
-                }
-            }
-        });
-    }
-    onMount(chart);
 </script>
 
 <div>
     <h1 class="text-xl"> This is the chart component </h1>
-    <canvas id="myChart"></canvas>
+    <div class="box-border h-16 w-96 border-4 rounded-lg"></div>
 </div>
