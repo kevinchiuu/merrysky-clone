7a4895e90b36e2fd86b04e99185be9d3057a3222
Kevin Chiu <keavenchiu@gmail.com>
2023-04-04 20:50:44 -0400

    css classes added

--- src/components/Chart.svelte
+++ src/components/Chart.svelte
@@ -4,5 +4,10 @@
 
 <div>
     <h1 class="text-xl"> This is the chart component </h1>
-    <div class="box-border h-16 w-96 border-4 rounded-lg"></div>
+    <div class="box-border h-14 w-96 rounded-lg m-0 flex">
+        <div class="bg-blue-500 h-full w-1/4 align-middle text-center rounded-l-lg"> <div class="h-full w-full text-center"> Rain </div> </div>
+        <div class="bg-slate-300 h-full w-1/4 justify-center text-center"> Cloudy </div>
+        <div class="bg-blue-500 h-full w-1/4 justify-center text-center"> Rain </div>
+        <div class="bg-slate-300 h-full w-1/4 justify-center text-center rounded-r-lg"> Cloudy </div>
+    </div>
 </div>
